e92eefe29b8215e8bfdd612867908a5f70fc3083
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-07-26 15:08:19 -0500

    chore(astro): setup project template

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [solidJs(), tailwind()],
+  output: "server",
+  adapter: netlify()
+});
