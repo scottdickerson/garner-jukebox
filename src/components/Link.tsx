20a7293f1efc68f831a47da96471b42a4712f0fb
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-07-27 16:19:26 -0500

    feat(animations): added audio and animations

---
+++ src/components/Link.tsx
@@ -0,0 +1,17 @@
+import type { JSX, ParentProps } from 'solid-js'
+import { delayedClick } from '../utils/utils'
+
+export const Link = ({
+    href,
+    children,
+    ...props
+}: ParentProps<{ href: string }>) => (
+    <a
+        {...props}
+        onClick={delayedClick(
+            () => (window.location.href = href?.toString() ?? '/')
+        )}
+    >
+        {children}
+    </a>
+)
---
+++ src/utils/utils.ts
@@ -0,0 +1,12 @@
+import type { HTMLAttributes } from 'astro/types'
+
+export const decades = ['1930s', '1940s', '1950s', '1960s', '1970s']
+
+export const delayedClick = (cb: () => void, delay = 3000) => {
+    return (event: MouseEvent) => {
+        event.preventDefault()
+        setTimeout(() => {
+            cb()
+        }, delay)
+    }
+}
