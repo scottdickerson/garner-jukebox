1fd1ed2567e30926112b46f140250fb9fe00e79c
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-07-27 16:28:27 -0500

    feat(pages): add base pages and static build (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
@@ -7,6 +7,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: "static",
+  adapter: netlify(),
 });
